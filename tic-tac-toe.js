cd7fef4ad852bfbcb118bd4c47ca8232d84eec1a
Lincoln-Brown <links1155@gmail.com>
2019-10-11 01:37:16 -0500

    exercise 2 and 3 done with out empty array

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,14 +1,39 @@
 
+
 function Main(){
  
 var divs=document.querySelector('#board');
-
+var turn = "X";
 
 for(i=0; i<divs.children.length; i++){
-divs.children[i].classList.add("square");	
+
+divs.children[i].classList.add("square");
+divs.children[i].onclick=function(){
+	
+if (turn==="X"||turn==="x"){
+		this.innerHTML = "<strong>X<strong>";
+		this.classList.add('X');
+		turn="O";
+}else{
+	this.innerHTML = "<strong>O<strong>";
+	this.classList.add('O');
+	turn="X";
 }
 
+};	//end of on click function 
+divs.children[i].addEventListener('mouseover',function(){
+this.classList.add('hover');
 
+});
+divs.children[i].addEventListener('mouseout',function(){
+this.classList.remove('hover');
+
+});
+
+
+
+		
+}// end of for loop 
 
 
 alert("DOMContentLoaded.");
